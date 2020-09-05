9568c85cf4fed143a69c06e851016e703a181556
Kappeh <kieranleep@hotmail.com>
2020-09-05 21:41:22 +0100

    moves node information away from transposition information

--- transpositiontable.go
+++ transpositiontable.go
@@ -3,11 +3,7 @@ package main
 import "sync"
 
 type TranspositionInfo struct {
-	// Score is an alpha value when a  lower bound
-	// Score is a  beta  value when an upper bound
-	Score       Boundary
-	SearchDepth int
-	PVMoveID    int
+	NodeInfo
 	Ancient     bool
 }
 
@@ -33,9 +29,8 @@ func (tt *TranspositionTable) MarkAllAncient() {
 	tt.lock.Lock()
 	for k, v := range tt.table {
 		tt.table[k] = TranspositionInfo{
-			Score:       v.Score,
-			SearchDepth: v.SearchDepth,
-			Ancient:     true,
+			NodeInfo: v.NodeInfo,
+			Ancient:  true,
 		}
 	}
 	tt.lock.Unlock()
@@ -46,9 +41,8 @@ func (tt *TranspositionTable) MarkNotAncient(h ZobristHash) {
 	v, ok := tt.table[h]
 	if ok {
 		tt.table[h] = TranspositionInfo{
-			Score:       v.Score,
-			SearchDepth: v.SearchDepth,
-			Ancient:     false,
+			NodeInfo: v.NodeInfo,
+			Ancient:  false,
 		}
 	}
 	tt.lock.Unlock()
@@ -75,36 +69,45 @@ func (tt *TranspositionTable) Probe(h ZobristHash) (TranspositionInfo, bool) {
 	return i, ok
 }
 
-func (tt *TranspositionTable) ProbeSymetry(s SymetryHashList) (TranspositionInfo, bool) {
-	var v  TranspositionInfo
-	var ok bool
+func (tt *TranspositionTable) ProbeSymetry(s SymetryHashList) (ZobristHash, bool) {
+	var hash ZobristHash
+	var ok   bool
 
 	tt.lock.RLock()
-	if v, ok = tt.table[s.Symetry1]; ok {
+	if _, ok = tt.table[s.Symetry1]; ok {
+		hash = s.Symetry1
 		goto Return
 	}
-	if v, ok = tt.table[s.Symetry2]; ok {
+	if _, ok = tt.table[s.Symetry2]; ok {
+		hash = s.Symetry2
 		goto Return
 	}
-	if v, ok = tt.table[s.Symetry3]; ok {
+	if _, ok = tt.table[s.Symetry3]; ok {
+		hash = s.Symetry3
 		goto Return
 	}
-	if v, ok = tt.table[s.Symetry4]; ok {
+	if _, ok = tt.table[s.Symetry4]; ok {
+		hash = s.Symetry4
 		goto Return
 	}
-	if v, ok = tt.table[s.Symetry5]; ok {
+	if _, ok = tt.table[s.Symetry5]; ok {
+		hash = s.Symetry5
 		goto Return
 	}
-	if v, ok = tt.table[s.Symetry6]; ok {
+	if _, ok = tt.table[s.Symetry6]; ok {
+		hash = s.Symetry6
 		goto Return
 	}
-	if v, ok = tt.table[s.Symetry7]; ok {
+	if _, ok = tt.table[s.Symetry7]; ok {
+		hash = s.Symetry7
 		goto Return
 	}
-	if v, ok = tt.table[s.Symetry8]; ok {
+	if _, ok = tt.table[s.Symetry8]; ok {
+		hash = s.Symetry8
 		goto Return
 	}
+	
 Return:
 	tt.lock.RUnlock()
-	return v, ok
+	return hash, ok
 }
